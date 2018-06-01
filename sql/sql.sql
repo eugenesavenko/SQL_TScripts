8022f4686055f60642e8a6dc29204be36481c924
Eugene Savenko <eugenesavenko@users.noreply.github.com>
2018-06-01 16:25:50 +0300

    2018.06.01.16.25.00

--- sql/sql.sql
+++ sql/sql.sql
@@ -2,4 +2,3 @@ SET ANSI_NULLS        ON                              ;
 SET NOCOUNT           ON                              ;
 SET QUOTED_IDENTIFIER ON                              ;
 SET TRANSACTION       ISOLATION LEVEL READ UNCOMMITTED;
-
