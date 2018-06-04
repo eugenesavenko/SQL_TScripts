36d2d5bac95d2529f0b58e6d018f26c9090ede79
Eugene Savenko <eugenesavenko@users.noreply.github.com>
2018-06-04 09:01:49 +0300

    2018.06.04.09.01.00

---
+++ view/set compatibility level on base.sql
@@ -0,0 +1,6 @@
+SET ANSI_NULLS        ON                              ;
+SET NOCOUNT           ON                              ;
+SET QUOTED_IDENTIFIER ON                              ;
+SET TRANSACTION       ISOLATION LEVEL READ UNCOMMITTED;
+
+ALTER DATABASE testdatabaseone SET COMPATIBILITY_LEVEL = 130;
---
+++ view/shrink file on base.sql
@@ -0,0 +1,6 @@
+SET ANSI_NULLS        ON                              ;
+SET NOCOUNT           ON                              ;
+SET QUOTED_IDENTIFIER ON                              ;
+SET TRANSACTION       ISOLATION LEVEL READ UNCOMMITTED;
+
+--USE [testdatabaseone]; DBCC SHRINKFILE (N'testdatabaseome_log' , 0);
